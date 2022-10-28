8a5460908c6f375f3498519d6121fe41895e01c7
Trebolll <M-or-ev@yandex.ru>
2022-10-28 23:29:18 +0700

    first commit and create ssl

---
+++ src/main/java/ru/javabegin/backend/hydrometcentr/HydrometcentrApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.backend.hydrometcentr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrometcentrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrometcentrApplication.class, args);
+    }
+
+}
